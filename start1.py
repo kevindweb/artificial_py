a511bccb5ef5fddbeb01539fb2934a30f1026c52
Kevin Deems <kevindwebsites@gmail.com>
2018-01-03 18:11:04 -0500

    Created AnalyzeCode class for looping contents only once

--- start1.py
+++ start1.py
@@ -1,32 +1,17 @@
-import os #for system commands
-import timeit #for function testing
-import re #regex split commands
-
-def write_next_file(contents, file):
-    # open and write file to cwd
-    fh = open(file,"w")
-    fh.write(contents)
+#for system commands
+import os
+#for function testing
+import timeit
+#regex split commands
+import re
 
 def run_next_file(contents, num):
-    file = "./start" + num + ".py"
-    write_next_file(contents, file)
+    file = "./start" + str(num) + ".py"
+    fh = open(file,"w")
+    fh.write(contents)
+    # call the next process to run and quit after process done
     os.system("python " + file)
-    print("finished on my own")
     quit()
-
-def is_number(s):
-    try:
-        float(s)
-        return True
-    except ValueError:
-        pass
-    try:
-        import unicodedata
-        unicodedata.numeric(s)
-        return True
-    except (TypeError, ValueError):
-        pass
-        return False
 
 def split_by_func(contents):
     func_list = {}
@@ -48,22 +33,9 @@ def split_by_func(contents):
     # return dictionary with function name and location in file
     return func_list
 
-def add_func(content, func_content):
-    content = content.split("\n")
-    for index,x in enumerate(content):
-        if "import" in x and not content[index + 1].split():
-            # adds the function after all the import statements
-            x += "\n\n" + func_content
-            content[index] = x
-            break
-    return "\n".join(content)
-
 def rewrite(contents, line, where, what, one_indent, functions=None):
     # python keywords that require indenting afterwards
     # where = where to put new code... (prepend = right after line; append = at the end of the conditional or func)
-    special_words = ["def","if","elif","else","try","except","for","class","with","while","finally"]
-    contents = contents.split("\n")
-    new_contents = []
     searching = False
     removeUntil = 0
     remove = False
@@ -115,37 +87,79 @@ def rewrite(contents, line, where, what, one_indent, functions=None):
             new_contents.append(x)
     return "\n".join(new_contents)
 
-def add_info(content, file_spec):
-    # names of the outputs of os.stat function
-    os_stat_list = ["MODE", "INO", "DEV", "NLINK", "UID", "GID", "SIZE", "ATIME", "MTIME", "CTIME", "FILENUM", "PID", "PREVFILENAME"]
-    file_name = file_spec[2]
-    file_spec = list(os.stat(file_name)) + file_spec
-    new_file_spec = "previous_file_spec = {"
-    for i,x in enumerate(file_spec):
-        x_key = "\"" + os_stat_list[i] + "\":"
-        x_val = str(x)
-        if is_number(x):
-            new_file_spec += x_key + x_val + ","
+
+class AnalyzeCode:
+    '''Runs through contents once - appending, analyzing, and deleting previous code'''
+    self.add_funcs = []
+    self.special_words = ["def","if","elif","else","try","except","for","class","with","while","finally"]
+    def __init__(self, c, f, num):
+        self.func_list = f
+        self.contents = c
+        # if this is the original file, don't remove duplicates (there are none)
+        self.remove_dup = (True if (num > 2) else False)
+
+    def is_number(n):
+        try:
+            float(n)
+            return True
+        except ValueError:
+            pass
+        try:
+            import unicodedata
+            unicodedata.numeric(n)
+            return True
+        except (TypeError, ValueError):
+            pass
+            return False
+
+    def add_info(self, file_spec):
+        os_stat_list = ["MODE", "INO", "DEV", "NLINK", "UID", "GID", "SIZE", "ATIME", "MTIME", "CTIME", "FILENUM", "PID", "PREVFILENAME"]
+        file_name = file_spec[2]
+        file_spec = list(os.stat(file_name)) + file_spec
+        new_file_spec = "previous_file_spec = {"
+        for i,x in enumerate(file_spec):
+            x_key = "\"" + os_stat_list[i] + "\":"
+            x_val = str(x)
+            if is_number(x):
+                new_file_spec += x_key + x_val + ","
+            else:
+                new_file_spec += x_key + "\"" + x_val + "\"}"
+        self.contents = self.contents.split("\n")
+        if self.contents[0][0] == "p":
+            self.contents[0] = new_file_spec
         else:
-            new_file_spec += x_key + "\"" + x_val + "\"}"
-    content = content.split("\n")
-    if content[0][0] == "p":
-        content[0] = new_file_spec
-    else:
-        content.insert(0, new_file_spec)
-    return "\n".join(content)
+            self.contents.insert(0, new_file_spec)
+        self.contents = "\n".join(self.contents)
+
+    def add_func(self, func_content):
+        self.add_funcs.append(func_content)
+
+    def run(self):
+        loop_content = self.contents.split("\n")
+        placeholder = []
+        for index,x in enumerate(loop_content):
+            placeholder.append(x)
+            # loop through everyline
+        self.contents = placeholder
+
+    __str__(self):
+        # simple return of the file_contents
+        return self.contents
+
 
 if __name__ == "__main__":
     file_num = int(re.findall(r'\d+',__file__)[0])
+    # here for a catch, so the files do not stack up in development
     if file_num > 3:
         quit()
     file_open = open(__file__,"r")
-    file_number = str(int(file_num) + 1)
+    file_number = int(file_num) + 1
     file_content = file_open.read()
     func_list = split_by_func(file_content)
+    go = AnalyzeCode(file_content, func_list, file_number)
     # file_content = rewrite(file_content, "def write_next_file", "prepend", "print(\"things\")", "    ", func_list)
     # file_content = rewrite(file_content, "if __name__ ==", "prepend", "newfile = open(\"newtext.txt\",\"w\");newfile.write(\"coolbeans\");newfile.close() if __file__ == 'start2.py' else quit() ", "    ")
-    file_content = rewrite(file_content, "if __name__ ==", "append", "stop_process(os.getpid())", "    ")
-    file_content = add_func(file_content, '''def stop_process(pid):\n    newfile = open(\"newtext.txt\",\"w\");newfile.write(str(pid));newfile.close()''')
+    # file_content = rewrite(file_content, "if __name__ ==", "append", "stop_process(os.getpid())", "    ")
+    # file_content = add_func(file_content, '''def stop_process(pid):\n    newfile = open(\"newtext.txt\",\"w\");newfile.write(str(pid));newfile.close()''')
     # file_content = add_info(file_content, [file_number, os.getpid(), __file__])
-    run_next_file(file_content, file_number)
+    # run_next_file(str(go), file_number)
